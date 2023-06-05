f87068bb4cea585768bc4841e9528b0227425bf3
AbhishekMashetty <abhishekmashetty97@gmail.com>
2023-06-05 16:10:25 -0700

    Added all the code

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+
+	"github.com/AbhishekMashetty/restaurant-system/database"
+
+	routes "github.com/AbhishekMashetty/restaurant-system/routes"
+
+	middleware "github.com/AbhishekMashetty/restaurant-system/middleware"
+
+	"github.com/gin-gonic/gin"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+}
